d7af7d8723ddb7126084035f6b8ee2f79e6cbc21
Aleksey <del-fisha@yandex.ru>
2023-02-03 12:59:10 +0300

    Done!

---
+++ Webinar GB/HomeTask/HomeTask1/Task1/src/main/java/org/example/Main.java
@@ -0,0 +1,55 @@
+//*1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
+//2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+//3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+//4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
+//
+//
+//int i = new Random().nextInt(k); //это кидалка случайных чисел!)
+
+package org.example;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int i, n;
+        n = 2;
+        Random random = new Random();
+        i = new Random().nextInt(2000);
+        System.out.println("i = " + i);
+        while(n < i/2) {
+            n *= 2;
+        }
+        System.out.println("n = " + n);
+        System.out.println("Short.MAX_VALUE:  " + Short.MAX_VALUE);
+        System.out.println("Short.MIN_VALUE: " + Short.MIN_VALUE);
+
+        int[] m1 = new int[(Short.MAX_VALUE - i)/n];
+        for (int j = 0; j < m1.length;){
+            for(int k = i;k < Short.MAX_VALUE; k++){
+                if(k % n == 0) {
+                    m1[j] = k;
+                    j++;
+                }
+                }
+            }
+        System.out.println("\nВсе кратные n числа в диапазоне от i до Short.MAX_VALUE в массиве:");
+        System.out.println(Arrays.toString(m1));
+
+        int[] m2 = new int[(i - Short.MIN_VALUE)-((i - Short.MIN_VALUE)/n)];
+        for (int j = 0; j < m2.length;){
+            for(int k = i;k > Short.MIN_VALUE; k--){
+                if(k % n != 0) {
+                    m2[j] = k;
+                    j++;
+                }
+            }
+        }
+        System.out.println("Все некратные n числа в диапазоне от Short.MIN_VALUE до i в массиве:");
+        System.out.println(Arrays.toString(m2));
+
+        System.out.println(m1.length);
+        System.out.println(m2.length);
+        }
+    }
