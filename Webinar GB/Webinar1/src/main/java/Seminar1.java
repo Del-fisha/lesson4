ee916213cd3f460bb0dac8fa6a4d162457776beb
Aleksey <del-fisha@yandex.ru>
2023-02-03 13:00:02 +0300

    Done!

---
+++ Webinar GB/Webinar1/src/main/java/Seminar1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Seminar1 {
+    public static void main(String[] args) {
+        String str = "Get me strong";
+        System.out.println(Arrays.toString(str.split("e")));
+    }
+}
