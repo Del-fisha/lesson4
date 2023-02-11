bf003eba8c1e39882de1891c3c5223a7c62417a8
Aleksey <del-fisha@yandex.ru>
2023-02-11 13:01:38 +0300

    Done first task + sum & mult

--- Webinar GB/HomeTask/HomeTask2/HomeTask1-1/src/main/java/org/example/Main.java
+++ Webinar GB/HomeTask/HomeTask2/HomeTask1-1/src/main/java/org/example/Main.java
@@ -1,22 +1,40 @@
 import java.util.Scanner;
 public class Main {
 
-    public  static  void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число: ");
         int number = scanner.nextInt();
-        if(isTriangular(number)) System.out.println("Число " + number + " является треугольным");
-        else System.out.println("Число " + number + " НЕ является треугольным");
-    }
+        int sum = TriangleSum(number), mult = TriangleMult(number);
 
-    static boolean isTriangular(int num){
-        if(num < 0) return false;
+        if (isTriangular(number)){
+            System.out.println("Число " + number + " является треугольным");
+            System.out.println("Сумма чисел: " + sum);
+            System.out.println("Произведение чисел: " + mult);
+        }
+        else System.out.println("Число " + number + " НЕ является треугольным");
+
+    }
+    static boolean isTriangular(int num) {
+        if (num < 0) return false;
         int sum = 0, n = 1;
         for (int i = 1; sum <= num; i++) {
             sum += i;
-            if(sum == num) return true;
+            if (sum == num) return true;
         }
         return false;
+    }
+    public static int TriangleSum(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return TriangleSum(num - 1) + num;
+    }
+    public static int TriangleMult(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return TriangleMult(num - 1) * num;
     }
 }
 
