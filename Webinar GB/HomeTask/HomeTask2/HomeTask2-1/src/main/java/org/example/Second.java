7b217224e12b54fb5dc41874fac1d5022f80e45f
Aleksey <del-fisha@yandex.ru>
2023-03-07 15:40:28 +0300

    done 2-2

---
+++ Webinar GB/HomeTask/HomeTask2/HomeTask2-1/src/main/java/org/example/Second.java
@@ -0,0 +1,18 @@
+//Напишите программу, чтобы проверить, являются ли две данные строки вращением друг друга (вхождение в обратном порядке)
+package org.example;
+
+import java.util.Scanner;
+
+public class Second {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите основную строку: ");
+        StringBuilder first = new StringBuilder(scan.nextLine());
+        System.out.println("Введите подстроку: ");
+        StringBuilder second = new StringBuilder(scan.nextLine()).reverse();
+
+
+        if(second.indexOf(String.valueOf(first)) >= 0) {System.out.println("Вхождение в обратном порядке присутствует!");}
+        else {System.out.println("Вхождение в обратном порядке отсутствует!");}
+    }
+}
