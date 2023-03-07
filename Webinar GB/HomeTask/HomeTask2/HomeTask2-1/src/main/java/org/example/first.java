bbe139bad58d9dfbe6b30a2890e3c9793fe46e82
Aleksey <del-fisha@yandex.ru>
2023-03-07 15:23:57 +0300

    done 2-1

---
+++ Webinar GB/HomeTask/HomeTask2/HomeTask2-1/src/main/java/org/example/first.java
@@ -0,0 +1,16 @@
+//Напишите программу, чтобы найти вхождение в строке (содержащей все символы другой строки).
+package org.example;
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите основную строку: ");
+        String first = scan.nextLine();
+        System.out.println("Введите подстроку: ");
+        String second = scan.nextLine();
+
+        if(second.contains(first)) {System.out.println("Подстрока содержится в строке!");}
+        else {System.out.println("Подстрока НЕ содержится в строке!");}
+    }
+}
