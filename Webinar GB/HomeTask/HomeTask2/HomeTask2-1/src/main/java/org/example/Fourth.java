df17e4914aa30b6464adb6e32837e57797606415
Aleksey <del-fisha@yandex.ru>
2023-03-12 16:28:55 +0300

    Done 2-4,5,6

--- Webinar GB/HomeTask/HomeTask2/HomeTask2-1/src/main/java/org/example/Fourth.java
+++ Webinar GB/HomeTask/HomeTask2/HomeTask2-1/src/main/java/org/example/Fourth.java
@@ -1,5 +1,7 @@
-//Дано два числа, например 3 и 56,
+// 4. Дано два числа, например 3 и 56,
 // необходимо составить следующие строки: 3 + 56 = 59 3 – 56 = -53 3 * 56 = 168 Используем метод StringBuilder.append().
+// 5. Замените символ “=” на слово “равно”. Используйте методы StringBuilder.insert(),StringBuilder.deleteCharAt().
+// 6. Замените символ “=” на слово “равно”. Используйте методы StringBuilder.replace().
 package org.example;
 
 public class Fourth {
@@ -28,6 +30,15 @@ public class Fourth {
                 .append(b)
                 .append(" = ")
                 .append(a * b);
-        System.out.println(diff);
+    }
+    public static StringBuilder replace1(StringBuilder str){
+        int del = str.indexOf("=");
+        str.deleteCharAt(del);
+        str.insert(del, "равно");
+        return str;
+    }
+    public static StringBuilder replace2(StringBuilder str){
+        str.replace(str.indexOf("="), str.indexOf("=") + 1, "равно" );
+        return str;
     }
 }
